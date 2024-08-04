e6a4b5c84e089bd89ecbdaeaec715e6df951589f
Sukanya Naik <snaik@ara.com>
2024-08-04 16:06:35 -0400

    demo

--- demo/src/main/java/com/example/demo/rest/HelloRestController.java
+++ demo/src/main/java/com/example/demo/rest/HelloRestController.java
@@ -1,10 +1,21 @@
 package com.example.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
+    @Value("${course.name}")
+    private String courseName;
+
     @GetMapping("/")
-    public String getGreeting() { return "Hello World!!!"; }
+    public String getGreeting() {
+        return "Hello World!!!";
+    }
+
+    @GetMapping("/courseName")
+    public String getProjectName() {
+        return courseName;
+    }
 }
