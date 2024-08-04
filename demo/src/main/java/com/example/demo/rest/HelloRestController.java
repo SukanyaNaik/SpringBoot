8ad9429e8765cef72f52b7698973f79ead300c92
Sukanya Naik <snaik@ara.com>
2024-08-04 13:11:47 -0400

    first commit

---
+++ demo/src/main/java/com/example/demo/rest/HelloRestController.java
@@ -0,0 +1,10 @@
+package com.example.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+    @GetMapping("/")
+    public String getGreeting() { return "Hello World!!!"; }
+}
